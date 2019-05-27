69f46d377abe6c1965d1808bf939607badea57b5
Mandi Grant <mandi018@gmail.com>
2019-05-27 10:51:09 -0500

    Built client, imported into server project

--- app.js
+++ app.js
@@ -144,7 +144,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/fetch-static-data', (req, res) => {
     fs.readFile('./results.json', 'utf8', function(err, data) {
---
+++ public/precache-manifest.52739ddf4bf20327bcb1a876927b0d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0a7674368a2e216a991682dab75868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ccb2e763262d22ad9dd",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f25d9bfb5a12fd8904f7",
+    "url": "/static/css/main.736cc352.chunk.css"
+  },
+  {
+    "revision": "1ccb2e763262d22ad9dd",
+    "url": "/static/js/2.ae6764ce.chunk.js"
+  },
+  {
+    "revision": "f25d9bfb5a12fd8904f7",
+    "url": "/static/js/main.23231241.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
